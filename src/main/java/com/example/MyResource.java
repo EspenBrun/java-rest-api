f7ccf3aa8ed03bb08528d294e8ba35575d340273
Espen K. Brun <espen_brun@hotmail.com>
2017-06-25 17:49:44 +0200

    GET shows info from db

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import jdk.nashorn.internal.runtime.ECMAException;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.SchemaOutputResolver;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.UUID;
 
@@ -17,11 +19,19 @@ import java.util.UUID;
 public class MyResource {
     private Connection connection;
     private Statement statement;
+    private String output;
+    private String query;
 
     @GET
     @Path("/list")
     public Response getMsg() {
-        String output = "Get:Jersey say :";
+        try {
+            dbConnect();
+            output = getPersonsAndCreateGetOutput();
+            dbClose();
+        } catch (Exception e){
+            output = e.toString();
+        }
         return Response.status(200).entity(output).build();
     }
 
@@ -29,12 +39,11 @@ public class MyResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response createMessage(@FormParam("name") String name, @FormParam("adress") String adress, @FormParam("phone") String phone){
         int result;
-        String output;
 
         try {
             dbConnect();
             result = dbInsert(name.trim(), adress.trim(), phone.trim());
-            output = postOutput(result);
+            output = createPostOutput(result);
             dbClose();
         } catch(Exception e){
             output = e.toString();
@@ -44,16 +53,37 @@ public class MyResource {
     }
 
     private int dbInsert(String name, String adress, String phone) throws Exception{
-        String query = "insert into triona_person values (null, '" + name + "', '" + adress + "', " + phone + ");";
+        query = "insert into triona_person values (null, '" + name + "', '" + adress + "', " + phone + ");";
         return statement.executeUpdate(query);
     }
 
-    private String postOutput(int result){
+    private String createPostOutput(int result){
         if (result == 0) {
             return "query wrong";
         } else {
             return "query executed";
         }
+    }
+
+    private String getPersonsAndCreateGetOutput() throws Exception{
+        ResultSet res = getPersons();
+        return createGetOutput(res);
+    }
+
+    private ResultSet getPersons() throws Exception {
+        query = "select * from triona_person;";
+        return statement.executeQuery(query);
+    }
+
+    private String createGetOutput(ResultSet res) throws Exception{
+        String s;
+        s = "Information about the persons:";
+        while(res.next()){
+            s += "\n\nName: " + res.getString("name") + "\n";
+            s += "Adress: " + res.getString("adress") + "\n";
+            s += "Phone number: " + res.getString("phone");
+        }
+        return s;
     }
 
     private void dbConnect() throws Exception{
@@ -72,5 +102,6 @@ public class MyResource {
         statement.close();
         connection.close();
     }
+
 }
 
